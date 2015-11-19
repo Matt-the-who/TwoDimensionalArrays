74cb77487e18349811150070474bb18c1c86bd59
Matt-the-who <matthew.ho@ocsbstudent.ca>
2015-11-19 14:43:22 -0500

    2d arrays

---
+++ TwoDimensionalArrays/TwoDArrays.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace TwoDimensionalArrays
+{
+    partial class frmTwoDArrays
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstBox = new System.Windows.Forms.ListBox();
+            this.btnEnter = new System.Windows.Forms.Button();
+            this.nudLength = new System.Windows.Forms.NumericUpDown();
+            this.nudWidth = new System.Windows.Forms.NumericUpDown();
+            this.lblSize = new System.Windows.Forms.Label();
+            this.lblAnswer = new System.Windows.Forms.Label();
+            this.mnuStrip = new System.Windows.Forms.MenuStrip();
+            this.mniFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.mniExit = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.nudLength)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudWidth)).BeginInit();
+            this.mnuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lstBox
+            // 
+            this.lstBox.FormattingEnabled = true;
+            this.lstBox.Location = new System.Drawing.Point(12, 36);
+            this.lstBox.Name = "lstBox";
+            this.lstBox.Size = new System.Drawing.Size(83, 225);
+            this.lstBox.TabIndex = 0;
+            // 
+            // btnEnter
+            // 
+            this.btnEnter.Location = new System.Drawing.Point(133, 124);
+            this.btnEnter.Name = "btnEnter";
+            this.btnEnter.Size = new System.Drawing.Size(75, 23);
+            this.btnEnter.TabIndex = 1;
+            this.btnEnter.Text = "Enter";
+            this.btnEnter.UseVisualStyleBackColor = true;
+            this.btnEnter.Click += new System.EventHandler(this.btnEnter_Click);
+            // 
+            // nudLength
+            // 
+            this.nudLength.Location = new System.Drawing.Point(114, 63);
+            this.nudLength.Name = "nudLength";
+            this.nudLength.Size = new System.Drawing.Size(120, 20);
+            this.nudLength.TabIndex = 2;
+            // 
+            // nudWidth
+            // 
+            this.nudWidth.Location = new System.Drawing.Point(114, 98);
+            this.nudWidth.Name = "nudWidth";
+            this.nudWidth.Size = new System.Drawing.Size(120, 20);
+            this.nudWidth.TabIndex = 3;
+            // 
+            // lblSize
+            // 
+            this.lblSize.AutoSize = true;
+            this.lblSize.Location = new System.Drawing.Point(111, 41);
+            this.lblSize.Name = "lblSize";
+            this.lblSize.Size = new System.Drawing.Size(127, 13);
+            this.lblSize.TabIndex = 4;
+            this.lblSize.Text = "Enter the size of the array";
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.AutoSize = true;
+            this.lblAnswer.Location = new System.Drawing.Point(111, 199);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(84, 13);
+            this.lblAnswer.TabIndex = 5;
+            this.lblAnswer.Text = "The average is: ";
+            // 
+            // mnuStrip
+            // 
+            this.mnuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mniFile});
+            this.mnuStrip.Location = new System.Drawing.Point(0, 0);
+            this.mnuStrip.Name = "mnuStrip";
+            this.mnuStrip.Size = new System.Drawing.Size(284, 24);
+            this.mnuStrip.TabIndex = 6;
+            this.mnuStrip.Text = "menuStrip1";
+            // 
+            // mniFile
+            // 
+            this.mniFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mniExit});
+            this.mniFile.Name = "mniFile";
+            this.mniFile.Size = new System.Drawing.Size(37, 20);
+            this.mniFile.Text = "File";
+            // 
+            // mniExit
+            // 
+            this.mniExit.Name = "mniExit";
+            this.mniExit.Size = new System.Drawing.Size(92, 22);
+            this.mniExit.Text = "Exit";
+            this.mniExit.Click += new System.EventHandler(this.mniExit_Click);
+            // 
+            // frmTwoDArrays
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 289);
+            this.Controls.Add(this.lblAnswer);
+            this.Controls.Add(this.lblSize);
+            this.Controls.Add(this.nudWidth);
+            this.Controls.Add(this.nudLength);
+            this.Controls.Add(this.btnEnter);
+            this.Controls.Add(this.lstBox);
+            this.Controls.Add(this.mnuStrip);
+            this.MainMenuStrip = this.mnuStrip;
+            this.Name = "frmTwoDArrays";
+            this.Text = "2D Arrays";
+            ((System.ComponentModel.ISupportInitialize)(this.nudLength)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudWidth)).EndInit();
+            this.mnuStrip.ResumeLayout(false);
+            this.mnuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstBox;
+        private System.Windows.Forms.Button btnEnter;
+        private System.Windows.Forms.NumericUpDown nudLength;
+        private System.Windows.Forms.NumericUpDown nudWidth;
+        private System.Windows.Forms.Label lblSize;
+        private System.Windows.Forms.Label lblAnswer;
+        private System.Windows.Forms.MenuStrip mnuStrip;
+        private System.Windows.Forms.ToolStripMenuItem mniFile;
+        private System.Windows.Forms.ToolStripMenuItem mniExit;
+    }
+}
+
---
+++ TwoDimensionalArrays/TwoDArrays.cs
@@ -0,0 +1,94 @@
+﻿/*
+Created by Matthew Ho
+Created on 11-19-15
+Created for ICS3U
+Assignment #5-05
+This program uses a 2d array to find the average of numbers in the array
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TwoDimensionalArrays
+{
+    public partial class frmTwoDArrays : Form
+    {
+        //Global
+        Random rnd = new Random();
+
+        public double AveragingFunction(double totalValue, int numberOfNumbers)
+        {
+            //This function averages things
+            double average = 0;
+
+            average = totalValue / numberOfNumbers;
+
+            return average;
+        }
+
+        public frmTwoDArrays()
+        {
+            InitializeComponent();
+        }
+
+        private void mniExit_Click(object sender, EventArgs e)
+        {
+            //Closes the application
+            this.Close();
+        }
+
+        private void btnEnter_Click(object sender, EventArgs e)
+        {
+            //Clear List
+            this.lstBox.Items.Clear();
+
+            //Variables
+            int lengthOfArray;
+            int widthOfArray;
+            double total = 0;
+            int randomNumber = 0;
+            int numberOfNumbers = 0;
+            double average = 0;
+
+            lengthOfArray = Convert.ToInt32(this.nudLength.Value);
+            widthOfArray = Convert.ToInt32(this.nudWidth.Value);
+
+            //The Array
+            int[,] arrayOfNumbers = new int[lengthOfArray, widthOfArray];
+
+            //For loop to assign each value in the array with a random number
+            for(int counterX = 0; counterX < lengthOfArray; counterX++)
+            {
+                for (int counterY = 0; counterY < widthOfArray; counterY++)
+                {
+                    randomNumber = rnd.Next(1, 11);
+                    arrayOfNumbers[counterX, counterY] = randomNumber;
+                    
+                    this.lstBox.Items.Add(randomNumber);
+                }
+            }
+
+            //For each loop to find total value and # of #s
+            foreach(int aValue in arrayOfNumbers)
+            {
+                total = total + aValue;
+
+                numberOfNumbers++;
+            }
+
+            //Calling function
+            average = AveragingFunction(total, numberOfNumbers);
+
+            //Output
+            this.lblAnswer.Text = "The average is: " + average;
+
+        }
+    }
+}
